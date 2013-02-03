099f96ae93b2a0ea924ec965f42d8e57dbd599f2
Russell Steicke <russells@adelie.cx>
2013-02-03 21:47:24 +0800

    t/20130203-external-bell-ring (2/2 commits) 20130203-214724.270723887 rlt4

--- external-bell.c
+++ external-bell.c
@@ -1,9 +1,41 @@
 #include "external-bell.h"
+#include "bsp.h"
 
 
 struct ExternalBell externalbell;
 
+
+static QState initialState(struct ExternalBell *me);
+static QState topState(struct ExternalBell *me);
+static QState offState(struct ExternalBell *me);
+
+
 void externalbell_ctor(struct ExternalBell *externalbell)
 {
+	QActive_ctor((QActive *)(&externalbell),
+		     (QStateHandler)&initialState);
+}
 
+
+static QState initialState(struct ExternalBell *me)
+{
+	return Q_TRAN(&offState);
+}
+
+
+static QState topState(struct ExternalBell *me)
+{
+	return Q_SUPER(&QHsm_top);
+}
+
+
+static QState offState(struct ExternalBell *me)
+{
+	switch (Q_SIG(me)) {
+	case Q_ENTRY_SIG:
+		BSP_buzzer(0);
+		return Q_HANDLED();
+	default:
+		return Q_SUPER(topState);
+	}
 }
